47a52af249204e1a020e82639f1ee71379e715c0
supervoidcoder <88671013+supervoidcoder@users.noreply.github.com>
2025-09-19 02:47:39 +0000

    gonna crash out

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-react-website/',
 })
 // not my first time using react but definitely first using vite
 // still can't tell the difference
